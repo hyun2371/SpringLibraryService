e0d368f7ed80c72f06815fbb5032f0e07ba8a210
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-04-02 20:39:41 +0900

    유저 조회 API

--- library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,7 +1,10 @@
 package com.group.libraryapp.controller.user;
 
+import com.group.libraryapp.domain.user.Fruit;
 import com.group.libraryapp.domain.user.User;
+import com.group.libraryapp.domain.user.response.UserResponse;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +20,20 @@ public class UserController {
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request){
         users.add(new User(request.getName(), request.getAge()));
+    }
+
+    @GetMapping("/fruit")
+    public Fruit fruit(){
+        return new Fruit("바나나", 2000);
+    }
+
+
+    @GetMapping("/user")
+    public List<UserResponse> getUsers(){
+        List<UserResponse> responses = new ArrayList<>();
+        for (int i = 0; i < users.size(); i++) {
+            responses.add(new UserResponse(i + 1, users.get(i)));
+        }
+        return responses;
     }
 }
