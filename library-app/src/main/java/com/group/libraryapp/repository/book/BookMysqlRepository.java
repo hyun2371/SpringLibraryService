0bd862bed20e70c0d864411fb427200882417d59
hs12 <77109954+hyun2371@users.noreply.github.com>
2023-04-06 19:19:59 +0900

    repository 교체 - 스프링 컨테이너 사용

--- library-app/src/main/java/com/group/libraryapp/repository/book/BookMysqlRepository.java
+++ library-app/src/main/java/com/group/libraryapp/repository/book/BookMysqlRepository.java
@@ -1,8 +1,11 @@
 package com.group.libraryapp.repository.book;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookMysqlRepository implements BookRepository{
     @Override
     public void saveBook() {
-
+        System.out.println("MySQLRepository");
     }
 }
